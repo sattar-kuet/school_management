316bb92645cb8c2b33965a288ec6585199c96b5a
Naznin Akter <naznin.kuet.te@gmail.com>
2023-09-21 14:26:22 +0600

    subject in list view fix

--- models/subject_config.py
+++ models/subject_config.py
@@ -7,5 +7,5 @@ class SubjectConfig(models.Model):
     _name = 'school_management.subject_config'
     _description = 'Subject Management'
 
-    class_config = fields.Many2one("school_management.class_config",string='Class Config', required=True, translate=True)
+    class_config = fields.Many2one("school_management.class_config", string='Class Config', required=True)
     subject = fields.Many2many("school_management.subject", string='Subject')
