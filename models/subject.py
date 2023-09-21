ce5f97e717bd9467a8c9bd07190305c40d904d69
Naznin Akter <naznin.kuet.te@gmail.com>
2023-09-21 15:48:26 +0600

    add subject vaia wizard

--- models/subject.py
+++ models/subject.py
@@ -11,3 +11,12 @@ class Subject(models.Model):
     has_practical = fields.Boolean(string='Has Practical')
     has_mcq = fields.Boolean(string='Has MCQ')
     has_written = fields.Boolean(string='Has Written')
+
+    def create(self):
+        return {
+            'name': 'Add New Subject',
+            'view_mode': 'form',
+            'res_model': 'school_management.subject.wizard',
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+        }
