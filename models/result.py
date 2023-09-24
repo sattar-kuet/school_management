9616771877d57d0c59764697914b19e2355188bf
Naznin Akter <naznin.kuet.te@gmail.com>
2023-09-24 21:41:12 +0600

    push update

--- models/result.py
+++ models/result.py
@@ -7,9 +7,10 @@ class Result(models.Model):
     _name = 'school_management.result'
     _description = 'Result Management'
 
-    exam = fields.Many2one('school_management.exam', string='Exam', required=True, translate=True)
+    exam = fields.Many2one('school_management.exam', string='Exam', required=True)
     student = fields.Many2one('res.user')
-    subject = fields.Many2one('school_management.subject', string='Subject', translate=True)
+    subject = fields.Many2one('school_management.subject', string='Subject')
     written_mark = fields.Char(string='Written Mark', required=True)
     mcq_mark = fields.Char(string='MCQ Mark', required=True)
     practical_mark = fields.Char(string='Practical Mark', required=True)
+
