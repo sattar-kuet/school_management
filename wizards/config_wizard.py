103b5464316863170edbada71fe81e0ccedd84a1
Naznin Akter <naznin.kuet.te@gmail.com>
2023-09-30 17:30:48 +0600

    push update

--- wizards/config_wizard.py
+++ wizards/config_wizard.py
@@ -13,7 +13,6 @@ class ConfigWizard(models.TransientModel):
     _name = 'school_management.config.wizard'
     _description = 'Config wizard'
 
-    
     written_pass_mark = fields.Float(string='Written Pass Mark')
     written_max_mark = fields.Float(string='Written Max Mark')
     mcq_pass_mark = fields.Float(string='MCQ Pass Mark')
@@ -21,26 +20,22 @@ class ConfigWizard(models.TransientModel):
     practical_pass_mark = fields.Float(string='Practical Pass Mark')
     practical_max_mark = fields.Float(string='Practical Max Mark')
 
+    subject_name = fields.Char(string='Subject Name')
     has_practical = fields.Boolean(compute="_has_practical")
     has_mcq = fields.Boolean(compute="_has_mcq")
     has_written = fields.Boolean(compute="_has_written")
     status = fields.Selection([('generated', 'System Generated'), ('configured', 'Configured')], defualt='generated')
 
-    
-    def _has_practical(self):
-        for record in self:
-            record.has_practical = record.subject.has_practical
+    @api.model
+    def default_get(self, fields):
+        this_wizard_fields = super(ConfigWizard, self).default_get(fields)
+        result_config = self.env['school_management.result_config'].browse(self.env.context.get('active_id'))
+        this_wizard_fields['has_mcq'] = result_config.subject.has_mcq
+        this_wizard_fields['has_written'] = result_config.subject.has_written
+        this_wizard_fields['has_practical'] = result_config.subject.has_practical
+        this_wizard_fields['subject_name'] = result_config.subject.title
 
-    
-    def _has_mcq(self):
-        for record in self:
-            record.has_mcq = record.subject.has_mcq
-
-    
-    def _has_written(self):
-        for record in self:
-            record.has_written = record.subject.has_written
+        return this_wizard_fields
 
     def result_config_button(self):
         pass
-
