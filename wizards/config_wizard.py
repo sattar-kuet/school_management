b95f891bd37a339d688779b192c97aea5a075cd1
Sajib <sojibali310@gmail.com>
2023-09-30 16:57:52 +0600

    config wizard add

--- wizards/config_wizard.py
+++ wizards/config_wizard.py
@@ -13,6 +13,8 @@ class ConfigWizard(models.TransientModel):
     _name = 'school_management.config.wizard'
     _description = 'Config wizard'
 
+    subject = fields.Many2one('school_management.combined_subject', string='Subject', required=True, relation='subject_combined_subject')
+    exam = fields.Many2many('school_management.exam', string='Exam')
     written_pass_mark = fields.Float(string='Written Pass Mark')
     written_max_mark = fields.Float(string='Written Max Mark')
     mcq_pass_mark = fields.Float(string='MCQ Pass Mark')
@@ -20,7 +22,26 @@ class ConfigWizard(models.TransientModel):
     practical_pass_mark = fields.Float(string='Practical Pass Mark')
     practical_max_mark = fields.Float(string='Practical Max Mark')
 
-    
+    has_practical = fields.Boolean(compute="_has_practical")
+    has_mcq = fields.Boolean(compute="_has_mcq")
+    has_written = fields.Boolean(compute="_has_written")
+    status = fields.Selection([('generated', 'System Generated'), ('configured', 'Configured')], defualt='generated')
+
+    @api.depends('subject.has_practical')
+    def _has_practical(self):
+        for record in self:
+            record.has_practical = record.subject.has_practical
+
+    @api.depends('subject.has_mcq')
+    def _has_mcq(self):
+        for record in self:
+            record.has_mcq = record.subject.has_mcq
+
+    @api.depends('subject.has_written')
+    def _has_written(self):
+        for record in self:
+            record.has_written = record.subject.has_written
 
     def result_config_button(self):
         pass
+
