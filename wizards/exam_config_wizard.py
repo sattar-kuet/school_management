9d90d7461f9f9a219cf243b9bcfa6a3ba9a25f78
Sajib <sojibali310@gmail.com>
2023-10-01 14:44:26 +0600

    push update

---
+++ wizards/exam_config_wizard.py
@@ -0,0 +1,24 @@
+import base64
+import io
+
+import requests
+import xlwt
+
+from odoo import fields, models, http, api
+from odoo.http import request
+from odoo.tools import ustr
+
+
+class ExamConfigWizard(models.TransientModel):
+    _name = 'school_management.exam_config.wizard'
+    _description = 'Exam Config wizard'
+
+    
+    type = fields.Selection([('new', 'New'), ('existing', 'Existing Config')])
+    existing_config = fields.Many2one('school_management.exam',string='Existing Config')
+
+
+    def exam_config(self):
+        pass
+
+    
