68d7e72605029b3bd535db5227e49776a13eef59
Naznin Akter <naznin.kuet.te@gmail.com>
2023-10-30 12:40:06 +0600

    session added done

--- models/session.py
+++ models/session.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api
+import datetime
 
 
 class SessionConfig(models.Model):
     _name = 'school_management.session'
     _description = 'Session Config'
+    _rec_name = 'title'
 
     title = fields.Char(string='Title')
-    start_year = fields.Integer(string='Start Year')
-    end_year = fields.Integer(string='End Year')
+    start_year = fields.Char(string='Start Year')
+    end_year = fields.Char(string='End Year')
     status = fields.Selection(
         selection=[('active', 'Active'),
                    ('archive', 'Archive')], string='Status', default='active')
@@ -17,6 +19,13 @@ class SessionConfig(models.Model):
     @api.model
     def default_get(self, fields):
         record = super(SessionConfig, self).default_get(fields)
-        record['title'] = 'test'
+        current_year = datetime.date.today().year
+        next_year = current_year + 1
+        current_year = str(current_year)
+        next_year = str(next_year)
+
+        record['title'] = f'Session: {current_year} - {next_year}'
+        record['start_year'] = current_year
+        record['end_year'] = next_year
 
         return record
