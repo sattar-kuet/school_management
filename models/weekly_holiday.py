e05976ac2f5ca905a84bb311f6a473095f7c6fea
Sajib <sojibali310@gmail.com>
2023-10-31 13:49:24 +0600

    update

--- models/weekly_holiday.py
+++ models/weekly_holiday.py
@@ -7,4 +7,4 @@ class WeeklyHolidayConfig(models.Model):
     _name = 'school_management.weekly.holiday'
     _description = 'Weekly Holiday Config'
 
-    week_days = fields.Many2many('school_management.week.day', string='Week Days')
+    weekly_off_days = fields.Many2many('school_management.week.day', string='Week Days')
