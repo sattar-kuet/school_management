d3904080c0695244336f4586d335d215619067a1
Naznin Akter <naznin.kuet.te@gmail.com>
2023-10-31 13:55:18 +0600

    push update

--- models/holiday.py
+++ models/holiday.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models
+import datetime
 
 
 class HolidayConfig(models.Model):
@@ -10,7 +11,15 @@ class HolidayConfig(models.Model):
     title = fields.Char(string='Title')
     start_date = fields.Char(string='Start Date')
     end_date = fields.Char(string='End Date')
-    session_id = fields.Many2one(compute='_compute_session_id', string='session')
 
-    def _compute_session_id(self):
-        pass
+    @staticmethod
+    def dates_for_year_range_and_specific_day(target_weekday, start_year, end_year):
+        date_list = []
+        for year in range(start_year, end_year + 1):
+            date = datetime.date(year, 1, 1)
+            while date.year == year:
+                if date.weekday() == target_weekday:
+                    date_list.append(date)
+                date += datetime.timedelta(days=1)
+
+        return date_list
