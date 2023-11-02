332a1c2b358a4a83962a88fc3ae73405da585355
Naznin Akter <naznin.kuet.te@gmail.com>
2023-11-02 22:43:34 +0600

    time filed component done

--- models/shift.py
+++ models/shift.py
@@ -7,6 +7,17 @@ class Shift(models.Model):
     _description = 'Shift'
 
     name = fields.Char(string='Name', required=True)
-    start_time = fields.Char(string='Start Time')
-    end_time = fields.Char(string='End Time')
+    start_time = fields.Char(string='Start Time', required=True)
+    start_time_am_pm = fields.Char(string='Start Time',compute='_compute_start_time')
+    end_time = fields.Char(string='End Time', required=True)
+    end_time_am_pm = fields.Char(string='End Time',compute='_compute_end_time')
 
+    def _compute_start_time(self):
+        for record in self:
+            time_obj = datetime.strptime(record.start_time, "%H:%M")
+            record.start_time_am_pm = time_obj.strftime("%I:%M %p")
+
+    def _compute_end_time(self):
+        for record in self:
+            time_obj = datetime.strptime(record.end_time, "%H:%M")
+            record.end_time_am_pm = time_obj.strftime("%I:%M %p")
