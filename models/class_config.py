c90d2cb0b946cb59d92ee82e8198795660866834
Sajib <sojibali310@gmail.com>
2023-11-14 15:32:31 +0600

    update

--- models/class_config.py
+++ models/class_config.py
@@ -5,16 +5,33 @@ from odoo import api
 
 
 class ClassConfig(models.Model):
-    _name = 'school_management.class_config'
+    _name = 'sm.class_config'
     _description = 'School Management'
 
     name = fields.Char(string='Name', required=True)
-    shift = fields.Many2one('school_management.shift')
-    setup_lines = fields.Many2many('school_management.class.setup.line')
+    shift = fields.Many2one('school_management.shift', String="Shift")
+    section = fields.Many2one('sm.section', String="Section")
+    setup_lines = fields.Many2many('sm.class.setup.line')
 
 
 class ClassSetupLine(models.Model):
-    _name = 'school_management.class.setup.line'
+    _name = 'sm.class.setup.line'
     _description = 'Class Setup line'
+
+
+    subject = fields.Many2one('school_management.subject', String="Subject")
+    teacher = fields.Many2one('res.users', String="Teacher")
+    class_room = fields.Many2one('school_management.class.room', String="Class Room")
+    start_at = fields.Char( String="Start at")
+    end_at = fields.Char( String="End at")
+    off_days = fields.Many2many('school_management.week.day', String="Off Days")
+
+
+class Section(models.Model):
+    _name = 'sm.section'
+    _description = 'Section'
+
+
+    name = fields.Char(String="Name")
 
 
