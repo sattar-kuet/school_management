914b1460028057abcc6fbe29ea2ff578cd974283
Naznin Akter <naznin.kuet.te@gmail.com>
2023-11-14 18:03:17 +0600

    class configuration done

--- models/class_config.py
+++ models/class_config.py
@@ -13,6 +13,31 @@ class ClassConfig(models.Model):
     shift = fields.Many2one('school_management.shift', string="Shift")
     section = fields.Many2one('sm.section', string="Section")
     setup_lines = fields.Many2many('sm.class.setup.line', string="Setup Line")
+    computed_setup_info = fields.Text(string='Configuration', compute='_compute_setup_info')
+
+    def _compute_setup_info(self):
+        for record in self:
+            html_output = '<ul>'
+            for setup_info in record.setup_lines:
+                off_days = ''
+                for off_day in setup_info.off_days:
+                    off_days += off_day.name + '  '
+                html_output += '<li>'
+                html_output += self.formatted_html('Subject',setup_info.subject.name)
+                html_output += self.formatted_html('Teacher',setup_info.teacher.name)
+                html_output += self.formatted_html('Class Room',setup_info.class_room.name)
+                html_output += self.formatted_html('Class Start Time',setup_info.start_at)
+                html_output += self.formatted_html('Class End Time',setup_info.end_at)
+                html_output += self.formatted_html('Off Days',off_days)
+                html_output += '</li>'
+            html_output += '</ul>'
+            record.computed_setup_info = html_output
+
+    @staticmethod
+    def formatted_html(title, value):
+        return f'<button style="margin: 2px;" type="button" class="btn btn-info">' \
+               f'{title} <span class="badge" style="background: #3c3737;font-size: 12px;padding: 4px;">' \
+               f'{value}</span></button>'
 
 
 class ClassSetupLine(models.Model):
