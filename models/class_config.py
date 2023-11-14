e5313215a36ddb6aeb85b691acf702dcdeccc8ba
Naznin Akter <naznin.kuet.te@gmail.com>
2023-11-14 12:37:14 +0600

    add subjects to class

--- models/class_config.py
+++ models/class_config.py
@@ -10,3 +10,10 @@ class ClassConfig(models.Model):
 
     name = fields.Char(string='Name', required=True)
     has_group = fields.Boolean(string='Has Group', required=True)
+    subjects = fields.Many2many('school_management.subject')
+
+    def _compute_subjects(self):
+        for record in self:
+            class_subject_config = self.env['school_management.subject_config'].search(
+                [('class_config', '=', record.id)], limit=1)
+            record.subjects = class_subject_config.subject.ids
