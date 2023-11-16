1fd9421169901931b0b78aa2f7e37fc08c8f197f
Naznin Akter <naznin.kuet.te@gmail.com>
2023-11-16 13:31:47 +0600

    subject choose

--- models/student_subject_config.py
+++ models/student_subject_config.py
@@ -7,5 +7,24 @@ class StudentSubjectConfig(models.Model):
     _description = 'Student Subject Config'
 
     student = fields.Many2one('res.users', string='Student')
-    subjects = fields.Many2one('school_management.subject', string='Subjects')
-    optional_subject = fields.Char(string='Optional Subject')
+    available_main_subjects = fields.Many2many('school_management.subject', compute='_compute_available_main_subjects')
+    main_subject = fields.Many2one('school_management.subject', string='Main Subjects',
+                                   domain="[('id', 'in', available_main_subjects)]")
+    optional_subject = fields.Many2one('school_management.subject', string='Optional Subjects')
+
+    @api.depends('student')
+    def _compute_available_main_subjects(self):
+        for record in self:
+            subject_ids = []
+            if record.student.class_has_group:
+                for setup_line in record.student.class_config.setup_lines:
+                    if not setup_line.subject.mandatory:
+                        subject_ids.append(setup_line.subject.id)
+            record.available_main_subjects = subject_ids
+
+    @api.onchange('main_subject')
+    def onchange_main_subject(self):
+        if self.main_subject:
+            return {'domain': {'optional_subject': [('id', '!=', self.main_subject.id)]}}
+        else:
+            return {'domain': {'optional_subject': []}}
