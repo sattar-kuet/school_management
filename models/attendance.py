6492e0100287699f02281b88d22003062ca0f6b3
Sajib <sojibali310@gmail.com>
2023-11-22 17:17:12 +0600

    attendance view

--- models/attendance.py
+++ models/attendance.py
@@ -10,3 +10,15 @@ class StudentAttendance(models.Model):
     present = fields.Boolean(string="Present", Default=False)
     effective_date = fields.Datetime(string="Date", default=lambda self: datetime.today())
     access_id = fields.Integer()
+    user_type = fields.Char('res.users', string="User Type", compute='_compute_user_type', store= True)
+
+
+    def _compute_user_type(self):
+        for record in self:
+            user_type = ''
+            if record.user:
+                if record.user.has_group('school_management.group_school_teacher'):
+                    user_type = 'teacher'
+                elif record.user.has_group('school_management.group_school_student'):
+                    user_type = 'student'
+            record.user_type = user_type
