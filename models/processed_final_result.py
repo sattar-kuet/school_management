235298859af6a373deefa67457430c6b176f9a02
Naznin Akter <naznin.kuet.te@gmail.com>
2023-11-23 15:26:03 +0600

    send sms on result publish

--- models/processed_final_result.py
+++ models/processed_final_result.py
@@ -12,8 +12,8 @@ class ProcessedFinalResult(models.Model):
     class_config = fields.Many2one('sm.class_config')
     student = fields.Many2one('res.users', string='Student', domain=lambda self: [
             ("groups_id", "in", [self.env.ref("school_management.group_school_student").id])])
-    grade_point = fields.Float(string='Grade Point')
-    total_marks = fields.Float(string='Total Mark')
+    grade_point = fields.Float(string='Grade Point', digits=(3, 2))
+    total_marks = fields.Float(string='Total Mark',digits=(6, 2))
     marks_in_percentage = fields.Float(string='Mark (%)')
     grade_title = fields.Char(string='Grade Title')
     status = fields.Selection([('published', 'Published'), ('archive', 'Archive')], default='published')
