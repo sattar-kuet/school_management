f02f23ae0ba189390195b2a45af038db1c0321ea
Naznin Akter <naznin.kuet.te@gmail.com>
2024-01-25 17:04:39 +0600

    push update

--- wizards/student_wizard.py
+++ wizards/student_wizard.py
@@ -1,5 +1,5 @@
 from odoo import fields, models, api
-
+from ..constants import BLOOD_GROUP
 
 class StudentWizard(models.TransientModel):
     _name = 'school_management.student.wizard'
@@ -7,16 +7,7 @@ class StudentWizard(models.TransientModel):
 
     name = fields.Char(string='Name', required=True)
     roll = fields.Char(string="Roll", required=True)
-    blood_group = fields.Selection([
-        ('A+', 'A+'),
-        ('A-', 'A-'),
-        ('B+', 'B+'),
-        ('B-', 'B-'),
-        ('AB+', 'AB+'),
-        ('AB-', 'AB-'),
-        ('O+', 'O+'),
-        ('O-', 'O-')
-    ], string='Blood Group', required=True)
+    blood_group = fields.Selection(BLOOD_GROUP, string='Blood Group', required=True)
     class_config = fields.Many2one("sm.class_config", string='Class')
     class_has_group = fields.Boolean(compute='_compute_class_has_group')
     student_group = fields.Many2one("school_management.group", string='Group')
@@ -33,6 +24,7 @@ class StudentWizard(models.TransientModel):
             student.class_has_group = student.class_config.has_group
 
     def add_student(self):
+        gaurdian_id = self.create_gaurdian()
         created_student = self.env['res.users'].create({
             'name': self.name,
             'roll': self.roll,
@@ -41,7 +33,9 @@ class StudentWizard(models.TransientModel):
             'student_group': self.student_group.id,
             'attendance_device_user_id': self.attendance_device_user_id,
             'phone': self.phone,
-            'guardian': self.guardian.id,
+            'guardian': gaurdian_id,
+            'batch': self.batch.id,
+            'sms_number': self.sms_number
         })
 
         student_group = self.env.ref('school_management.group_school_student')
@@ -49,6 +43,8 @@ class StudentWizard(models.TransientModel):
             created_student.write({
                 'groups_id': [(4, student_group.id, 0)]
             })
+
+        self.add_student_to_batch(created_student.id)
 
         action = {
             'name': 'Student',
@@ -60,3 +56,24 @@ class StudentWizard(models.TransientModel):
             'target': 'current',
         }
         return action
+    
+    def create_gaurdian(self):
+        created_guardian = self.env['res.users'].create({
+            'name': self.guardian_name,
+            'phone': self.guardian_number
+        })
+        guardian_group = self.env.ref('school_management.group_school_guardian')
+        if guardian_group:
+            created_guardian.write({
+                'groups_id': [(4, guardian_group.id, 0)]
+            })
+        return created_guardian.id
+    
+    def add_student_to_batch(self,student_id):
+        student_ids = self.batch.students.ids
+        student_ids.append(student_id)
+        batch_obj = self.env['school_management.batch'].browse(self.batch.id)
+        batch_obj.write({
+            'students': [(6, 0, student_ids)]
+        })
+
